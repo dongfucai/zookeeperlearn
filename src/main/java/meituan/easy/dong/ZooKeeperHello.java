f1eb98172326ffdce8d6bf16d5cf649b6729f373
dongfucai <dongfucai@meituan.com>
2018-09-30 16:05:01 +0800

    fist zookeeper  lock queue

---
+++ src/main/java/meituan/easy/dong/ZooKeeperHello.java
@@ -0,0 +1,43 @@
+package meituan.easy.dong;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.IOException;
+
+/**
+ * @Package Name : ${PACKAG_NAME}
+ * @Author : 1766318592@meituan.com
+ * @Creation Date : 2018年09月14日上午11:21
+ * @Function : todo
+ */
+public class ZooKeeperHello {
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+
+        ZooKeeper zk = new ZooKeeper("127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183", 30000, new DemoWatcher());//连接zk server
+        String node = "/app1";
+        Stat stat = zk.exists(node, false);//检测/app1是否存在
+        if (stat == null) {
+            //创建节点
+            String createResult = zk.create(node, "test".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            System.out.println(createResult);
+        }
+        //获取节点的值
+        byte[] b = zk.getData(node, false, stat);
+        System.out.println(new String(b));
+        zk.close();
+    }
+
+    static class DemoWatcher implements Watcher {
+       @Override
+        public void process(WatchedEvent event) {
+            System.out.println("----------->");
+            System.out.println("path:" + event.getPath());
+            System.out.println("type:" + event.getType());
+            System.out.println("stat:" + event.getState());
+            System.out.println("<-----------");
+        }
+    }
+
+}
